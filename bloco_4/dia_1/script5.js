0593ee8aee84fa7f2ec1ed89d0e7db42efdea4e9
Matheus Carvalho <matheuscarvalhoscm@gmail.com>
2021-05-19 19:34:00 -0300

    #5 exercice resolution

---
+++ bloco_4/dia_1/script5.js
@@ -0,0 +1,15 @@
+// define triângulo
+
+let a1 = -61;
+let a2 = 60;
+let a3 = 60;
+
+if (a1 == 60 && a2 == 60 && a3 == 60) {
+    console.log(true);
+
+} else if (a1 < 0  || a2 < 0 || a3 < 0) {
+    console.log ('erro');
+    
+} else {
+    console.log(false);
+}
