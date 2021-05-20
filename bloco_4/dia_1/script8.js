fec9e1936dc1bfa1e350b99a57d879ed04904ad7
Matheus Carvalho <matheuscarvalhoscm@gmail.com>
2021-05-19 22:02:42 -0300

    #8 exercice resolution

---
+++ bloco_4/dia_1/script8.js
@@ -0,0 +1,11 @@
+// retorna se existe número par
+
+let num1 = 1;
+let num2 = 2;
+let num3 = 3;
+
+if ((num1 % 2) === 0 || (num2 % 2) === 0 || (num3 % 2) === 0) {
+    console.log(true)
+} else {
+    console.log(false)
+}
