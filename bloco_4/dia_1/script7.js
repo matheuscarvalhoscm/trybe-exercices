0d1382809ed33a1c89a78d0e3fb5e9eb41e4c3b2
Matheus Carvalho <matheuscarvalhoscm@gmail.com>
2021-05-19 21:55:46 -0300

    #7 exercice correction

--- bloco_4/dia_1/script7.js
+++ bloco_4/dia_1/script7.js
@@ -36,30 +36,22 @@ switch(nota) {
 
 */
 
-let nota = -1;
+let nota = 75;
 
-if (nota >= 90) {
+if (nota < 0 || nota > 100) {
+    console.log('erro')
+
+} else if (nota >= 90) {
     console.log('A');
-
 } else if (nota >= 80) {
     console.log('B');
-
 } else if (nota >= 70) {
     console.log('C');
-
-} else if (nota >= 70) {
-    console.log('D');
-
 } else if (nota >= 60) {
-    console.log('E');
-
-} else if (nota < 50 && nota > 0) {
-    console.log('F');
-
-} else if (nota <= 0 || nota >= 100) {
-    console.log ('erro')
-
+    console.log('D');
+} else if (nota >= 50) {
+    console.log('E')
 } else {
-    console.log('N/A')
-
+    console.log('F')
 }
+
