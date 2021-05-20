e03abae0807966da98f9c5d86662b61bbbde7de5
Matheus Carvalho <matheuscarvalhoscm@gmail.com>
2021-05-19 22:06:32 -0300

    #9 exercice resolution

---
+++ bloco_4/dia_1/script9.js
@@ -0,0 +1,11 @@
+// retorna se existe número ímpar
+
+let num1 = 1;
+let num2 = 2;
+let num3 = 3;
+
+if ((num1 % 2) === 1 || (num2 % 2) === 1 || (num3 % 2) === 1) {
+    console.log(true)
+} else {
+    console.log(false)
+}
