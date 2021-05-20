c612d8845eae3500fca881d616089b2bb19e782d
Matheus Carvalho <matheuscarvalhoscm@gmail.com>
2021-05-19 21:57:24 -0300

    #7 exercice indentation correction

--- bloco_4/dia_1/script7.js
+++ bloco_4/dia_1/script7.js
@@ -1,46 +1,7 @@
-// converte porcentagem em conceito A-F
-
-/* 
-let nota1 = 90%
-let nota2 = 80%
-let nota3 = 70%
-let nota4 = 60%
-let nota5 = 50%
-
-let nota = 90%;
-
-switch(nota) {
-    case 90%:
-        console.log('A');
-        break;
-
-    case 80%:
-        console.log('B');
-        break;
-            
-    case 70%:
-        console.log('C');
-        break;
-
-    case 60%:
-        console.log('D');
-        break;
-    
-    case 50%:
-        console.log('F');
-        break;
-
-    default nota < 0 || nota > 100:
-        console.log('erro')
-}
-
-*/
-
 let nota = 75;
 
 if (nota < 0 || nota > 100) {
     console.log('erro')
-
 } else if (nota >= 90) {
     console.log('A');
 } else if (nota >= 80) {
