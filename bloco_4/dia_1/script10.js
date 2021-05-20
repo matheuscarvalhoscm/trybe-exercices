974bce304c591f1bf56469ef4e66cc9fa301dfd1
Matheus Carvalho <matheuscarvalhoscm@gmail.com>
2021-05-19 22:25:22 -0300

    #10 exercice resolution

---
+++ bloco_4/dia_1/script10.js
@@ -0,0 +1,10 @@
+// calcula lucro de uma empresa
+
+let productCost = 1000;
+let taxes = productCost * 0.2;
+let totalProductCost = productCost + taxes; 
+let salePrice = 2500;
+let profit = salePrice - totalProductCost;
+
+console.log('O lucro nesta venda foi de ' + profit);
+
