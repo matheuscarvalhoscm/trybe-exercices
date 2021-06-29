620eeaf928a6d5f3c7972412f76dde3d43fc62bf
Matheus Carvalho <matheuscarvalhoscm@gmail.com>
2021-06-29 18:29:31 -0300

    8.5 lecture

---
+++ bloco_8/dia_5/lecture/array-destructuting.js
@@ -0,0 +1,40 @@
+//  ex 1
+const saudacoes = ['Olá', (saudacao) => console.log(saudacao)];
+
+saudacoes[1](saudacoes[0]); // Olá
+
+// Produza o mesmo resultado acima, porém utilizando array destructuring
+
+const [saudacao, funcao] = saudacoes;
+
+console.log(funcao(saudacao));
+
+// ------------------------------------------------
+
+// ex 2
+let comida = 'gato';
+let animal = 'água';
+let bebida = 'arroz';
+
+console.log(comida, animal, bebida); // arroz gato água
+
+// Utilizando array destructuring, faça com que os valores apareçam nas variáveis correspondentes ao seu verdadeiro tipo
+
+[animal, bebida, comida] = [comida, animal, bebida];
+
+console.log(animal);
+console.log(bebida);
+console.log(comida);
+
+//  -------------------------------------------------------
+
+// ex 3
+let numerosPares = [1, 3, 5, 6, 8, 10, 12];
+
+console.log(numerosPares); // [6, 8, 10, 12];
+
+// Utilize array destructuring para produzir o resultado esperado pelo console.log abaixo
+
+[,,, ...numerosPares] = numerosPares;
+
+console.log(numerosPares);
