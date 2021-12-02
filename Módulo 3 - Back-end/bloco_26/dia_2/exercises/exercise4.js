43060da867c137e1a3fadad1896cf6522378b2b1
matheuscarvalhoscm <matheuscarvalhoscm@gmail.com>
2021-12-02 00:17:39 -0300

    create file for exercise 5

--- Módulo 3 - Back-end/bloco_26/dia_2/exercises/exercise4.js
+++ Módulo 3 - Back-end/bloco_26/dia_2/exercises/exercise4.js
@@ -110,27 +110,3 @@ const addMaggieToSimpsonFamily = async () => {
 };
 
 // addMaggieToSimpsonFamily();
-
-const createFiles = async () => {
-  const fileNames = ["Finalmente", "estou", "usando", "Promise.all", "!!!"];
-
-  const promiseFiles = [];
-  fileNames.forEach((file, index) => {
-    const newFile = fs.writeFile(`file${index + 1}.txt`, `${file}`, "utf8");
-    promiseFiles.push(newFile);
-  });
-
-  const allFiles = [];
-  promiseFiles.forEach((_, index) => {
-    allFiles.push(fs.readFile(`file${index + 1}.txt`, "utf8"))
-  });
-
-  const fileAllText = await Promise.all(allFiles);
-  
-  await fs.writeFile('fileAll.txt', fileAllText.join(' '));
-
-  console.log(`Arquivo fileAll.txt criado com sucesso!`);
-
-};
-
-createFiles();
