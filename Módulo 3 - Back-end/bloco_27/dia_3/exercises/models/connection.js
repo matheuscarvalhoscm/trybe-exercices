9276caec6a511073e187fce26f9ec73d024a57ff
matheuscarvalhoscm <matheuscarvalhoscm@gmail.com>
2022-01-05 19:24:17 -0300

    hide

--- Módulo 3 - Back-end/bloco_27/dia_3/exercises/models/connection.js
+++ Módulo 3 - Back-end/bloco_27/dia_3/exercises/models/connection.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2/promise');
 const connection = mysql.createPool({
   host: 'localhost', // Se necessário, substitua pelo seu host, `localhost` é o comum
   user: 'test', // Se necessário, substitua pelo seu usuário para conectar ao banco na sua máquina
-  password: 'Summer.03', // Se necessário, substitua pela sua senha para conectar ao banco na sua máquina
+  password: '****', // Se necessário, substitua pela sua senha para conectar ao banco na sua máquina
   database: 'rest_exercicios'});
 
 module.exports = connection;
