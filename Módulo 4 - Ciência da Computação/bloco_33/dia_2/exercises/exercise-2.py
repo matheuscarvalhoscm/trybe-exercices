691b73a3783cdd14e417889be59b003873e526b9
matheuscarvalhoscm <matheuscarvalhoscm@gmail.com>
2022-03-21 21:08:56 -0300

    33.2 exercises

---
+++ Módulo 4 - Ciência da Computação/bloco_33/dia_2/exercises/exercise-2.py
@@ -0,0 +1,18 @@
+# Exercise 2:
+import random
+
+
+words = ["batata", "cebola", "tomate"]
+random_word = random.choice(words)
+scrambled_word = "".join(random.sample(random_word, len(random_word)))
+print(scrambled_word)
+number_of_tries = 0
+
+while number_of_tries < 3:
+  user_word = input("Tente adivinha a palavra embaralhada: ")
+  if user_word == random_word:
+    print("Você acertou!")
+    break
+  else:
+    print(f"Errou... tente novamente")
+    number_of_tries += 1
